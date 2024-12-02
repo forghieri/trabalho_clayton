38440a9f917ffc93e192eb091d9b198a545c0e36
Igor F <forghieri14@gmail.com>
2024-12-02 11:55:35 -0300

    remocao de pastas

--- paises_continentes/src/Main.java
+++
@@ -1,8 +0,0 @@
-import controller.MainController;
-
-public class Main {
-    public static void main(String[] args) {
-        MainController controller = new MainController();
-        controller.mostrarMenu();
-    }
-}
